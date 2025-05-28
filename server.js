dcc5ffe3fc1666cc478ef0501461993bc9421da8
untoni <37137032+untoni@users.noreply.github.com>
2025-05-28 13:52:03 +0200

    clear comments

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-const express = require('express');         // якщо "type":"module" в package.json
-// або const express = require('express');
+const express = require('express'); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Сервінг статичних файлів з папки public
 app.use(express.static('public'));
 
-// Опціонально — додатковий API-роут
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from Node.js backend!' });
 });
